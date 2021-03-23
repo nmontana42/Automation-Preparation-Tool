7d16350b184c40484705d4971c6e86dd986ab1f6
Nick Montana <nick42m@gmail.com>
2021-03-23 18:24:01 -0400

    Fix errors

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,10 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+}
 });
