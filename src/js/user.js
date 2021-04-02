2ee875478abb87916f551340ba52a1611bdedc8e
Nick Montana <nick42m@gmail.com>
2021-04-02 16:02:20 -0400

    Querying improved

--- src/js/user.js
+++ src/js/user.js
@@ -4,24 +4,37 @@ import { listJobRisks } from "../graphql/queries";
 import '../scss/user.scss';
 Amplify.configure(awsmobile);
 
-async function query(){
-    const TableJobRiskFilterInput = {
-        Occupation: {
-            contains: "Engineer"
-        }
-    };
-    return await API.graphql(graphqlOperation(listJobRisks, {filter: TableJobRiskFilterInput}));
+//Analyze class queries based off form input
+class Analyze{
+    constructor(job){
+        this.job = job;
+    }
+    risk(){
+        const TableJobRiskFilterInput = {
+            Occupation: {
+                contains: this.job
+            }
+        };
+        return API.graphql(graphqlOperation(listJobRisks, {filter: TableJobRiskFilterInput}));
+        
+    }
 }
+const queryResult = document.getElementById('Results');
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('form').onsubmit = () => {
+        const job = document.querySelector('#job').value;
+        var Probability = new Analyze(job);
+        Probability.risk().then((evt => {
+            evt.data.listJobRisks.items.map((item) => {
+                queryResult.innerHTML += `<p>${item.Occupation} - ${item.Probability}</p>`;
+            });
+        }))
+        return false;
+    }
+});
 
 
-const input = document.getElementById("testquery")
-const queryResult = document.getElementById("QueryResults")
 
-document.addEventListener('DOMContentloaded', function() {
-    input.addEventListener("click", query().then((evt) => {
-        evt.data.listJobRisks.items.map((item) => {
-            queryResult.innerHTML += `<p>${item.Occupation} - ${item.Probability}</p>`;
-        });
-    }));
-})
 
